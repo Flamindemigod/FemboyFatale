a2927647827b79a779bac2252d22d96f48dcd110
Flamindemigod <23692454+Flamindemigod@users.noreply.github.com>
2025-08-04 23:40:49 +0100

    [FFUtils] wrote a small logger

--- FemboyFataleUtils.lua
+++ FemboyFataleUtils.lua
@@ -9,6 +9,28 @@ function FFUtils.Iota()
     end
 end
 
+function FFUtils.Logger(moduleName, traceMode)
+    -- traceMode is a u2
+    -- 0 No logging
+    -- 1 info
+    -- 2 debug
+    -- 3 trace
+    return {
+        info = function(msg)
+            if (traceMode <= 1) then return nil end
+            CHAT_SYSTEM:AddMessage("[" .. moduleName .. "] Info: " .. msg)
+        end,
+        debug = function(msg)
+            if (traceMode <= 2) then return nil end
+            CHAT_SYSTEM:AddMessage("[" .. moduleName .. "] Debug: " .. msg)
+        end,
+        trace = function(msg)
+            if (traceMode <= 3) then return nil end
+            CHAT_SYSTEM:AddMessage("[" .. moduleName .. "] Trace: " .. msg)
+        end
+    }
+end
+
 -- Straight up yoinked from Buff the Group
 -- https://github.com/Fostecks/BuffTheGroup
 -- Repo hasnt been updated for a while
