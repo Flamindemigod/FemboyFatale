b59e9a26ec75180f705b42a4df5165ebedd4ff92
Flamindemigod <23692454+Flamindemigod@users.noreply.github.com>
2025-08-04 23:45:28 +0100

    [FFTextures]: added a few more icons, hooked up the logger, ran formatter, and made more changes to data struct

--- modules/FFTextures.lua
+++ modules/FFTextures.lua
@@ -6,46 +6,58 @@ local s = FFTextures
 s.name = r.name .. "Textures"
 s.displayName = "FFTextures"
 s.defaults = {enabled = false}
--- Funky Hashmap
-local overrides = {}
-
+s.debug = 1
 s.data = {
-    overrides = {
+    rev = {},
+    textures = {
         {
             abilityId = 61771,
-            override = "FemboyFatale/icons/overrides/AstoPA.dds"
-        }
+            overrides = {
+                "FemboyFatale/icons/overrides/AstoPA.dds",
+                "FemboyFatale/icons/overrides/AstoTransmutation.dds"
+            }
+        }, -- Powerful Assualt
+        {
+            abilityId = 76936,
+            overrides = {"FemboyFatale/icons/overrides/AstoTransmutation.dds"}
+        }, -- Robes of Transmutation
+        {
+            abilityId = 166731,
+            overrides = {"FemboyFatale/icons/overrides/AstoRC.dds"}
+        } -- Rallying Cry
     }
 }
 
-for idx, v in pairs(s.data.overrides) do
+for idx, v in pairs(s.data.textures) do
     v.source = GetAbilityIcon(v.abilityId)
     v.abilityName = GetAbilityName(v.abilityId)
+    s.data.rev[v.abilityId] = idx
     s.defaults[idx] = {enabled = false}
 end
 
-local function reloadTexture(texture_id)
-    local texture = s.data.overrides[texture_id]
-    if (s.sv.enabled and s.sv[texture_id].enabled) then
-        overrides[texture.abilityId] = texture_id
-        RedirectTexture(texture.source, texture.override)
-    else
-        overrides[texture.abilityId] = nil
-        RedirectTexture(texture.source, texture.source)
+local function reloadTexture(abilityId)
+    local old_texture = s.data.textures[s.data.rev[abilityId]].source
+    RedirectTexture(old_texture, old_texture)
+    if (s.sv.enabled and s.sv[abilityId] ~= nil) then
+        local new_texture =
+            s.data.textures[s.data.rev[abilityId]].overrides[s.sv[abilityId]]
+        RedirectTexture(old_texture, new_texture)
     end
+    s.logger.trace("Loaded Texture for " .. abilityId)
 end
 
 function s.reset()
-    for _, v in pairs(s.data.overrides) do
-        RedirectTexture(v.source, v.source)
-        overrides = {}
-    end
+    for _, v in pairs(s.data.textures) do RedirectTexture(v.source, v.source) end
+    s.logger.trace("Reset All Textures")
     s.init()
 end
 
 function s.init()
+    s.logger = FFUtils.Logger(s.displayName, s.debug)
+    s.logger.trace("Initialized Logger")
     if (s.sv.enabled ~= true) then return end
-    for idx, _ in pairs(s.data.overrides) do reloadTexture(idx) end
+    for _, v in pairs(s.data.textures) do reloadTexture(v.abilityId) end
+    s.logger.trace("Initialized Textures")
 end
 
 function s.menu(root)
@@ -69,32 +81,36 @@ function s.menu(root)
         }
     }
     local controls = parent.controls
-    for idx, v in pairs(s.data.overrides) do
+    for _, v in pairs(s.data.textures) do
         controls[#controls + 1] = {
             type = "submenu",
             name = "|c00FFCC" .. string.upper(v.abilityName) .. "|r",
-            controls = {
-                {
-                    type = "checkbox",
-                    name = "Enable/Disable",
-                    getFunc = function()
-                        return s.sv[idx].enabled
-                    end,
-                    setFunc = function(enable)
-                        s.sv[idx].enabled = enable
-                        reloadTexture(idx)
-                    end
-                }
-
-            },
-            icon = function()
-                return s.sv[idx].enabled and v.override or v.source
-            end,
-            disabled = function()
-                return not s.sv.enabled and overrides[v.abilityId] ~= nil and
-                           overrides[abilityId] ~= idx
-            end
+            controls = {},
+            icon = v.source
         }
+        local c = controls[#controls].controls
+        for idx, tex in pairs(v.overrides) do
+            c[#c + 1] = {
+                type = "texture",
+                image = tex,
+                imageWidth = 64,
+                imageHeight = 64,
+                width = "half"
+            }
+            c[#c + 1] = {
+                type = "checkbox",
+                name = "Enable/Disable",
+                getFunc = function()
+                    return s.sv[v.abilityId] == idx
+                end,
+                setFunc = function(val)
+                    s.sv[v.abilityId] = val and idx or nil
+                    s.logger.trace("Setting Texture for " .. v.abilityId)
+                    reloadTexture(v.abilityId)
+                end,
+                width = "half"
+            }
+        end
     end
     root[#root + 1] = parent
 end
